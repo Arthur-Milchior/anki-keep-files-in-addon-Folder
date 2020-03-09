d862ff409a08b74c560b6669528056c90adeeaf9
Arthur Milchior <Arthur@Milchior.fr>
2020-03-09 09:52:47 +0100

    pep8 and sort

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .config import getUserOption
 
 defaultBaseNames = {"user_files", ".git", ".gitignore", ".svn"}
 
+
 def backupUserFiles(self, sid):
     """Move user file's folder to a folder called files_backup in the add-on folder"""
     if not os.path.exists(self._userFilesBackupPath()):
@@ -13,16 +14,19 @@ def backupUserFiles(self, sid):
         if os.path.exists(p):
             os.rename(p, os.path.join(self._userFilesBackupPath(), filename))
 
+
 def restoreUserFiles(self, sid):
     """Move the back up of user file's folder to its normal location in
     the folder of the addon sid"""
-    
+
     for filename in getUserOption("base names", defaultBaseNames):
         if os.path.exists(os.path.join(self._userFilesBackupPath(), filename)):
-            os.rename(os.path.join(self._userFilesBackupPath(), filename), os.path.join(self.addonsFolder(sid), filename))
+            os.rename(os.path.join(self._userFilesBackupPath(), filename),
+                      os.path.join(self.addonsFolder(sid), filename))
 
     if os.path.exists(self._userFilesBackupPath()):
         os.rmdir(self._userFilesBackupPath())
+
 
 AddonManager.backupUserFiles = backupUserFiles
 AddonManager.restoreUserFiles = restoreUserFiles
